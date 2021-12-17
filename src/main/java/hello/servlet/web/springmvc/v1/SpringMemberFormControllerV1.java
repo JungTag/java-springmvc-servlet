fb96dcf9563542d92e00cbd49b53c268312596e6
JungTag <dbsdudxor121@naver.com>
2021-12-17 13:56:41 +0900

    feat: Spring MVC 구조

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+// @Controller = Component + RequestMapping -> 핸들러 등록
+@Component
+@RequestMapping // 핸들러 매핑 + 어댑터 매핑
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
